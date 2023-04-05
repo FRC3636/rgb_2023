20c71435cee5c20f22862d616cc4e70f943550de
birdsoftheworld <exoticbirdsoftheworld@gmail.com>
2023-04-05 04:00:40 +0100

    fix: rename files correctly

--- settings.py
+++ settings.py
@@ -1,153 +1,8 @@
-from util import color
-from util.section import Layout, Section, Part, PartTransform
+from util.section import Layout, Section
 from default import *
-
-from patterns.rainbow import Rainbow
-from patterns.moving import Moving
-from patterns.solid import Solid
-from patterns.hybrid import Hybrid
-from patterns.scale import Scaled, ScaledTo
-from patterns.breathe import Breathe
-from patterns.cycle import Cycle
-from patterns.gradient import Gradient
-from patterns.just import Just
-from patterns.noise import Noise
-from patterns.memory import Memory
-from patterns.rain import Rain
-from patterns.choice import Choice
-from patterns.timed import Timed
-from patterns.smooth import Smooth
-from patterns.sample import Sample
-from patterns.automata import Automata
-from patterns.conditional import Conditional
-from patterns.ranged import Ranged
-
-def _update(a, b):
-    updated = False
-    for k in b:
-        if b[k] != a.get(k):
-            updated = True
-    a.update(b)
-    return updated
 
 class Settings:
     def __init__(self):
-        self.presets = {
-            "cube": Solid(
-                 color.magenta
-	    ),
-            "cone": Solid(
-                color.Color(255, 64, 0)
-            ),
-            "breathe": Breathe(
-                Solid(
-                    color.blue
-                )
-            ),
-            "worms": Moving(
-                Scaled(
-                    Hybrid(
-                        Solid(color.Color(127, 127, 127)),
-                        Solid(color.Color(0, 0, 10))
-                    ),
-                    1/4
-                ),
-                speed = 3/4
-            ),
-            "noise_rain": Rain(
-                Memory(
-                    Noise(value = 0.7, saturation = 1)
-                )
-            ),
-            "rainbow": Moving(
-                Scaled(
-                    Rainbow(),
-                    1/2
-                )
-            ),
-            "whole_rainbow": Just(
-                Moving(
-                    Rainbow(),
-                    1/4
-                )
-            ),
-            "rainbow_snakes":
-            Moving(
-                Scaled(
-                    Hybrid(
-                        Rainbow(),
-                        Solid(color.black)
-                    ),
-                    1/4
-                )
-            ),
-            "rainbow_snakes2": Moving(
-                Scaled(
-                    Hybrid(
-                        Moving(
-                            Rainbow(),
-                            speed = -1/3
-                                * 2 # from hybrid
-                                * 4 # from scaled
-                        ),
-                        Solid(color.black)
-                    ),
-                    1/4
-                )
-            ),
-            "rainbow_rain": Rain(
-                Moving(
-                    Rainbow()
-                )
-            ),
-            "fire": Timed(
-                Smooth(
-                    Memory(
-                        Sample(
-                            Gradient(color.yellow, color.Color(255, 64, 0), color.Color(255, 64, 0), color.Color(255, 64, 0), color.red)
-                        )
-                    ),
-                    time = 1/16
-                ),
-                frequency = 16
-            ),
-            "fire2": Timed(
-                Smooth(
-                    Memory(
-                        Sample(
-                            Gradient(color.blue.multiply(0.75), color.green.multiply(0.75))
-                        )
-                    ),
-                    time = 1/16
-                ),
-                frequency = 16
-            ),
-            "debug_five": Conditional(
-                lambda pos: (pos.ipos % 5) == 0, Gradient(color.red, color.blue)
-            ),
-            "none": Solid(color.black),
-            "solid_red": Solid(color.red),
-            "solid_yellow": Solid(color.yellow),
-            "solid_green": Solid(color.green),
-            "solid_cyan": Solid(color.cyan),
-            "solid_blue": Solid(color.blue),
-            "solid_magenta": Solid(color.magenta),
-            "solid_white": Solid(color.white),
-            "armrainbow": Just(
-                Moving(
-                    Gradient(color.red, color.yellow, color.green, color.cyan, color.magenta, color.red),
-                    1/4
-                )
-            ),
-            "armrainbow2": Just(
-                Moving(
-                    Gradient(color.red, color.yellow, color.green, color.cyan, color.magenta, color.red),
-                    1/4
-                ),
-                7
-            )
-        }
-
         self.properties = {
             "enabled": True
         }
@@ -209,8 +64,7 @@ class Settings:
                     Part(BODY5),
                     Part(BODY6),
                     Part(BODY7),
-                    Part(BODY8),
-                    Part(BODY9)
+                    Part(BODY8)
                 ),
                 Section(
                     self.presets[uarm_pattern],
--- util/section.py
+++ util/section.py
@@ -10,36 +10,13 @@ def _flatten(l):
     return l[:1] + _flatten(l[1:])
 
 
-class Layout:
-    def __init__(self, *sections):
-        self.sections = sections
-
-    def pattern(self):
-        ranges = []
-        for section in self.sections:
-            l = 0
-            for part in section.parts:
-                l += part.length()
-            start = 0
-            for part in section.parts:
-                length = part.length()
-                ranges.append(((part.start, part.end), (start, l, part.transform.apply(section.pattern))))
-                start += length
-        ranges.sort(key=lambda x: x[0][0])
-        r = MappedRange(*ranges)
-        return Ranged(r)
-
-
-class Section:
-    def __init__(self, pattern, *parts):
-        self.pattern = pattern
-        self.parts = parts
-
-
-class PartTransform:
+class SectionTransform:
     def __init__(self, translate=0, reverse=False):
         self.translate = translate
         self.reverse = reverse
+
+    def plus(self, other):
+        return SectionTransform(self.translate + other.translate, self.reverse ^ other.reverse)
 
     def apply(self, pattern):
         if self.reverse:
@@ -48,11 +25,81 @@ class PartTransform:
             return Manipulated(pattern, lambda pos: pos.translate(self.translate))
 
 
-class Part:
-    # exclusive of upper bound
-    def __init__(self, range, transform=PartTransform()):
-        self.start, self.end = range
+class Section:
+    def __init__(self, transform=SectionTransform()):
         self.transform = transform
+    
+    def compute_range(self):
+        raise "unimplemented"
 
-    def length(self):
-        return self.end - self.start
+
+class MultiSection(Section):
+    def __init__(self, *children, **kwargs):
+        super().__init__(**kwargs)
+        self.children = children
+        
+        
+class SectionSpec:
+    def __init__(self, section, start, length):
+        self.section = section
+        self.start = start
+        self.length = length
+
+
+class CombinedSection(MultiSection):
+    def __init__(self, pattern, length, *sections, **kwargs):
+        self.sections = sections
+        super().__init__(*map(lambda x: x.section, sections), **kwargs)
+        self.pattern = pattern
+        self.length = length
+    
+    def compute_range(self):
+        ranges = []
+        start = 0
+        for section in self.sections:
+            ranges.append(((part.start, part.end), (start, l, part.transform.apply(section.pattern))))
+            start += length
+        ranges.sort(key=lambda x: x[0][0])
+        r = MappedRange(*ranges)
+        return Ranged(r)
+
+    @classmethod
+    def ordered(pattern, *children, **kwargs):
+        length = 0
+        positioned_children = []
+        for child in children:
+            positioned_children.append(SectionSpec(length, child))
+            length += child.length
+        return CombinedSection(pattern, length, *positioned_children, **kwargs)
+
+
+class PartSection(Section):
+    def __init__(self, part, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.part = part
+    
+    def compute_range(self):
+        return [ _SectionData(self).transform_by(self.transform) ]
+
+
+class _SectionData:
+    def __init__(self, part, transform=SectionTransform()):
+        self.part = part
+        self.transform = transform
+        self.pattern = None
+        self.pos = 0
+    
+    def with_pattern(self, pattern):
+        self.pattern = pattern
+        return self
+    
+    def transform_by(self, transform):
+        self.transform = self.transform.plus(transform)
+        return self
+        
+    def add(self, val):
+        self.pos += val
+        return self
+    
+    def pattern(self):
+        return self.transform.apply(self.pattern)
